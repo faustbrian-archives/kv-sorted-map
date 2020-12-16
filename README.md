b8dc5f9dfa2c2b8775f1cb8b10601970507e5975
Brian Faust <hello@basecode.sh>
2020-12-16 13:31:18 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# @konceiver/kv-sorted-map
+
+<p align="center"><img src="./banner.png" /></p>
+
+[![Latest Version](https://badgen.now.sh/npm/v/@konceiver/kv-sorted-map)](https://www.npmjs.com/package/@konceiver/kv-sorted-map)
+[![License: MIT](https://badgen.now.sh/badge/license/MIT/green)](./LICENSE)
+[![Build Status](https://img.shields.io/github/workflow/status/konceiver/kv-sorted-map/run-tests?label=tests)](https://img.shields.io/github/workflow/status/konceiver/kv-sorted-map/CI?label=CI)
+[![Total Downloads](https://badgen.net/npm/dt/konceiver/kv-sorted-map)](https://npmjs.org/package/@konceiver/kv-sorted-map)
+
+## Installation
+
+```bash
+yarn add @konceiver/kv-sorted-map
+```
+
+## Testing
+
+```bash
+yarn test
+```
+
+## Security
+
+If you discover a security vulnerability within this package, please send an e-mail to security@konceiver.dev. All security vulnerabilities will be promptly addressed.
+
+## Credits
+
+This project exists thanks to all the people who [contribute](../../contributors).
+
+## License
+
+This is free and open source software under the terms of the [MIT License](./LICENSE).
---
+++ src/sync.ts
@@ -0,0 +1,101 @@
+// tslint:disable: no-unsafe-any
+import { IKeyValueStoreSync } from "@konceiver/kv";
+// @ts-ignore
+// tslint:disable-next-line: no-submodule-imports
+import SortedMap from "collections/sorted-map";
+
+export class StoreSync<K, T> implements IKeyValueStoreSync<K, T> {
+	private constructor(private readonly store: SortedMap<K, T>) {}
+
+	public static new<K, T>(): StoreSync<K, T> {
+		return new StoreSync<K, T>(new SortedMap<K, T>());
+	}
+
+	public all(): [K, T][] {
+		return [...this.store.entries()];
+	}
+
+	public keys(): K[] {
+		return [...this.store.keys()];
+	}
+
+	public values(): T[] {
+		return [...this.store.values()];
+	}
+
+	public get(key: K): T | undefined {
+		return this.store.get(key);
+	}
+
+	public getMany(keys: K[]): (T | undefined)[] {
+		return [...keys].map((key: K) => this.get(key));
+	}
+
+	public pull(key: K): T | undefined {
+		const item: T | undefined = this.get(key);
+
+		this.forget(key);
+
+		return item;
+	}
+
+	public pullMany(keys: K[]): (T | undefined)[] {
+		const items: (T | undefined)[] = this.getMany(keys);
+
+		this.forgetMany(keys);
+
+		return items;
+	}
+
+	public put(key: K, value: T): boolean {
+		this.store.set(key, value);
+
+		return this.has(key);
+	}
+
+	public putMany(values: [K, T][]): boolean[] {
+		return values.map((value: [K, T]) => this.put(value[0], value[1]));
+	}
+
+	public has(key: K): boolean {
+		return this.store.has(key);
+	}
+
+	public hasMany(keys: K[]): boolean[] {
+		return [...keys].map((key: K) => this.has(key));
+	}
+
+	public missing(key: K): boolean {
+		return !this.has(key);
+	}
+
+	public missingMany(keys: K[]): boolean[] {
+		return [...keys].map((key: K) => this.missing(key));
+	}
+
+	public forget(key: K): boolean {
+		return this.store.delete(key);
+	}
+
+	public forgetMany(keys: K[]): boolean[] {
+		return [...keys].map((key: K) => this.forget(key));
+	}
+
+	public flush(): boolean {
+		this.store.clear();
+
+		return this.count() === 0;
+	}
+
+	public count(): number {
+		return this.store.length;
+	}
+
+	public isEmpty(): boolean {
+		return this.count() === 0;
+	}
+
+	public isNotEmpty(): boolean {
+		return !this.isEmpty();
+	}
+}
